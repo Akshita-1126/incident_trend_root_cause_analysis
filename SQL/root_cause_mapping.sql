0c16754b12e83688a7d03821d03ee9e7c5e7b587
Akshita Kashyap <kashyapakshita31@gmail.com>
2026-03-19 19:55:08 +0530

    Update root_cause_mapping.sql

--- SQL/root_cause_mapping.sql
+++ SQL/root_cause_mapping.sql
@@ -25,7 +25,7 @@ INSERT INTO root_cause_mapping VALUES
 
 select * from root_cause_mapping;
 
-select a.incident_id, b.root_cause
+select a.incident_id,a.closed_code, b.root_cause
 from incident_data a
 Join root_cause_mapping b
 on a.closed_code = b.code;
